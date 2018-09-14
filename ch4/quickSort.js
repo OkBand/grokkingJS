bd927e42c4280ab494ee25ff265e37aa2355ae5a
Bandura <o.bandura@itransition.com>
2018-09-14 19:41:07 +0300

    quickSort edited

--- ch4/quickSort.js
+++ ch4/quickSort.js
@@ -4,12 +4,12 @@ function quickSort(arr){
   let less = [];
   let more = [];
   for(let i = 1; i < arr.length; i++) {
-    if (arr[i] <= pivot) {less.push(arr[i]);}
-    else {more.push(arr[i]);}
+    if (arr[i] <= pivot) less.push(arr[i]);
+    else more.push(arr[i]);
   }
 
   return quickSort(less).concat(pivot).concat(quickSort(more));
 }
 
 
-console.log(quickSort([2,8,6,12,3,4]));
+console.log(quickSort([2,8,6,12,3,4,4,15,55,43,42,42,888,8,8,7,2,0]));
